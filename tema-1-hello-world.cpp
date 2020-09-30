09cc7607b7fe960b14393db2f7c72f530fcf2898
Víctor Manuel Rivas Santos <vrivas@users.noreply.github.com>
2020-09-30 17:58:59 +0200

    Quito algunas líneas en blanco innecesarias

--- tema-1-hello-world.cpp
+++ tema-1-hello-world.cpp
@@ -1,18 +1,15 @@
 /******************************************************************************
-
                               Online C++ Compiler.
                Code, Compile, Run and Debug C++ program online.
 Write your code in this editor and press "Run" button to compile and execute it.
-
 *******************************************************************************/
 // Escrito y compilado desde https://www.onlinegdb.com/online_c++_compiler
-#include <iostream>
 
+#include <iostream>
 using namespace std;
 
 int main()
 {
     cout<<"Hello World";
-
     return 0;
 }
